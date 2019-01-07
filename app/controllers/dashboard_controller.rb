1ba0446b99726f6f15f81cf4c883106ab9723655
Biswanath Chandramouli <biju.mouli@gmail.com>
2019-01-07 20:56:12 +0530

    Added a dashboard for visitors and new movies can be created only by logged in users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,17 @@
 class DashboardController < ApplicationController
   def index
+  	@movies = Movie.all
+
+    @categories = @movies.uniq.pluck(:category)
+    @movies_by_category = Hash.new
+
+    @categories.each do |category|
+      @movies_by_category[category] = Movie.where(:category => category).length
+    end   
+
+    respond_to do |format|
+      format.html
+      format.json { render json: UsersDatatable.new(view_context)} 
+    end  
   end
 end
